4018f09cd4cbacbdc9c6fb95f88577a0137e475b
superblocks <superblocks@github.com>
2013-09-06 09:00:46 +0000

    add checkpoint for block #15000

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Copyright (c) 2011-2012 Litecoin Developers
+// Copyrgiht (c) 2013 The Pokercoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -30,6 +31,7 @@ namespace Checkpoints
             // Block 2913 contains the transaction that DESTROYED the 100,000 POK Premine from Block 2.
             // The entire premine was sent to the destroyer address: 2BuCidDstqL7xDforAjhFxcuStL1V5jzVRa
             ( 2913, uint256("0x702fb5ca8bd664e61fe2d39a99c6a4730d48cfa29cb3e8d4bda2f9612ca925b6"))
+            ( 15000, uint256("0xf7eee4141ebe793cf756f9d0e84b922ba32ef409cd6b279863905710021c8f97"))
             ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
